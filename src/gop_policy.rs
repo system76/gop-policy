7a81d0c8e96333077bd1517c1e3a252f8fd0c019
Jeremy Soller <jeremy@system76.com>
2019-05-03 13:46:38 -0600

    Add gop-policy implementation

---
+++ src/gop_policy.rs
@@ -0,0 +1,84 @@
+use core::ops::Try;
+use uefi::Handle;
+use uefi::boot::InterfaceType;
+use uefi::guid::{Guid, NULL_GUID};
+use uefi::memory::PhysicalAddress;
+use uefi::status::{Error, Result, Status};
+
+static VBT: &'static [u8] = include_bytes!(env!("FIRMWARE_OPEN_VBT"));
+
+pub static GOP_POLICY_GUID: Guid = Guid(0xec2e931b,0x3281,0x48a5,[0x81,0x7,0xdf,0x8a,0x8b,0xed,0x3c,0x5d]);
+pub const GOP_POLICY_REVISION: u32 = 0x03;
+
+#[repr(C)]
+pub enum LidStatus {
+    LidClosed,
+    LidOpen,
+    LidStatusMax,
+}
+
+#[repr(C)]
+pub enum DockStatus {
+    Docked,
+    UnDocked,
+    DockStatusMax,
+}
+
+extern "win64" fn GetPlatformLidStatus(CurrentLidStatus: *mut LidStatus) -> Status {
+    if CurrentLidStatus.is_null() {
+        return Status::from_error(Error::InvalidParameter);
+    }
+
+    // TODO: Get real lid status
+    unsafe { *CurrentLidStatus = LidStatus::LidOpen };
+
+    Status(0)
+}
+
+extern "win64" fn GetVbtData(VbtAddress: *mut PhysicalAddress, VbtSize: *mut u32) -> Status {
+    if VbtAddress.is_null() || VbtSize.is_null() {
+        return Status::from_error(Error::InvalidParameter);
+    }
+
+    unsafe { *VbtAddress = PhysicalAddress(VBT.as_ptr() as u64) };
+    unsafe { *VbtSize = VBT.len() as u32 };
+
+    Status(0)
+}
+
+extern "win64" fn GetPlatformDockStatus(CurrentDockStatus: DockStatus) -> Status {
+    Status::from_error(Error::Unsupported)
+}
+
+#[repr(C)]
+pub struct GopPolicy {
+    pub Revision: u32,
+    pub GetPlatformLidStatus: extern "win64" fn (CurrentLidStatus: *mut LidStatus) -> Status,
+    pub GetVbtData: extern "win64" fn (VbtAddress: *mut PhysicalAddress, VbtSize: *mut u32) -> Status,
+    pub GetPlatformDockStatus: extern "win64" fn (CurrentDockStatus: DockStatus) -> Status,
+    pub GopOverrideGuid: Guid,
+}
+
+impl GopPolicy {
+    pub fn new() -> Box<Self> {
+        Box::new(Self {
+            Revision: GOP_POLICY_REVISION,
+            GetPlatformLidStatus,
+            GetVbtData,
+            GetPlatformDockStatus,
+            GopOverrideGuid: NULL_GUID,
+        })
+    }
+
+    pub fn install(self: Box<Self>) -> Result<()> {
+        let uefi = unsafe { std::system_table_mut() };
+
+        let self_ptr = Box::into_raw(self);
+        let mut handle = Handle(0);
+        (uefi.BootServices.InstallProtocolInterface)(&mut handle, &GOP_POLICY_GUID, InterfaceType::Native, self_ptr as usize)?;
+
+        //let _ = (uefi.BootServices.UninstallProtocolInterface)(handle, &GOP_POLICY_GUID, self_ptr as usize);
+
+        Ok(())
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#![no_std]
+// #![feature(asm)]
+// #![feature(const_fn)]
+// #![feature(core_intrinsics)]
+#![feature(prelude_import)]
+#![feature(try_trait)]
+#![allow(non_snake_case)]
+
+#[macro_use]
+extern crate uefi_std as std;
+
+#[allow(unused_imports)]
+#[prelude_import]
+use std::prelude::*;
+
+use core::ops::Try;
+use uefi::status::Status;
+
+mod gop_policy;
+
+#[no_mangle]
+pub extern "C" fn main() -> Status {
+    let gop_policy = gop_policy::GopPolicy::new();
+    if let Err(err) = gop_policy.install() {
+        println!("GopPolicy error: {:?}", err);
+        Status::from_error(err)
+    } else {
+        Status(0)
+    }
+}
