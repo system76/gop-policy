e7930102c90b8385bfd31991e76a75136069744c
Tim Crawford <tcrawford@system76.com>
2023-11-21 08:29:48 -0700

    Replace redox_uefi with r-efi
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,40 @@
 
 #![no_std]
 #![no_main]
-#![feature(try_trait_v2)]
 #![allow(non_snake_case)]
 
-#[macro_use]
-extern crate uefi_std as std;
+// XXX: ??? `st` *is* in an `unsafe` block
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 
-use std::prelude::*;
-
-use core::ops::FromResidual;
-use std::uefi::status::Status;
+extern crate alloc;
 
 mod gop_policy;
 
-#[no_mangle]
-pub extern "C" fn main() -> Status {
-    let gop_policy = gop_policy::GopPolicy::new();
-    if let Err(err) = gop_policy.install() {
-        println!("GopPolicy error: {:?}", err);
-        Status::from_residual(err)
-    } else {
-        Status(0)
+use alloc::boxed::Box;
+use r_efi::efi;
+
+#[export_name = "efi_main"]
+pub extern "efiapi" fn main(_h: efi::Handle, st: *mut efi::SystemTable) -> efi::Status {
+    let mut handle: efi::Handle = core::ptr::null_mut();
+    let mut guid = gop_policy::GOP_POLICY_GUID;
+
+    let policy = gop_policy::GopPolicy::new();
+    let ptr = Box::into_raw(policy) as *mut core::ffi::c_void;
+
+    unsafe {
+        ((*(*st).boot_services).install_protocol_interface)(
+            &mut handle,
+            &mut guid,
+            efi::NATIVE_INTERFACE,
+            ptr,
+        )
     }
 }
+
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: r_efi_alloc::global::Bridge = r_efi_alloc::global::Bridge::new();
