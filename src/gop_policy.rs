e3d808112c95ccd02f1d9be2089fa3c1f6248683
Tim Crawford <tcrawford@system76.com>
2024-06-10 11:13:59 -0600

    Update redox_uefi_std
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/gop_policy.rs
+++ src/gop_policy.rs
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-use core::ops::FromResidual;
 use std::prelude::*;
-use std::uefi::Handle;
 use std::uefi::boot::InterfaceType;
-use std::uefi::guid::{Guid, NULL_GUID};
 use std::uefi::memory::PhysicalAddress;
-use std::uefi::status::{Error, Result, Status};
 
 static VBT: &[u8] = include_bytes!(env!("FIRMWARE_OPEN_VBT"));
 
-pub static GOP_POLICY_GUID: Guid = Guid(0xec2e931b,0x3281,0x48a5,[0x81,0x7,0xdf,0x8a,0x8b,0xed,0x3c,0x5d]);
+pub static GOP_POLICY_GUID: Guid = guid!("ec2e931b-3281-48a5-8107-df8a8bed3c5d");
 pub const GOP_POLICY_REVISION: u32 = 0x03;
 
 #[allow(unused)]
@@ -31,18 +27,18 @@ pub enum DockStatus {
 
 extern "win64" fn GetPlatformLidStatus(CurrentLidStatus: *mut LidStatus) -> Status {
     if CurrentLidStatus.is_null() {
-        return Status::from_residual(Error::InvalidParameter);
+        return Status::INVALID_PARAMETER;
     }
 
     // TODO: Get real lid status
     unsafe { *CurrentLidStatus = LidStatus::LidOpen };
 
-    Status(0)
+    Status::SUCCESS
 }
 
 extern "win64" fn GetVbtData(VbtAddress: *mut PhysicalAddress, VbtSize: *mut u32) -> Status {
     if VbtAddress.is_null() || VbtSize.is_null() {
-        return Status::from_residual(Error::InvalidParameter);
+        return Status::INVALID_PARAMETER;
     }
 
     unsafe { *VbtAddress = PhysicalAddress(VBT.as_ptr() as u64) };
@@ -52,7 +48,7 @@ extern "win64" fn GetVbtData(VbtAddress: *mut PhysicalAddress, VbtSize: *mut u32
 }
 
 extern "win64" fn GetPlatformDockStatus(_CurrentDockStatus: DockStatus) -> Status {
-    Status::from_residual(Error::Unsupported)
+    Status::UNSUPPORTED
 }
 
 #[repr(C)]
@@ -71,7 +67,7 @@ impl GopPolicy {
             GetPlatformLidStatus,
             GetVbtData,
             GetPlatformDockStatus,
-            GopOverrideGuid: NULL_GUID,
+            GopOverrideGuid: Guid::NULL,
         })
     }
 
@@ -80,7 +76,12 @@ impl GopPolicy {
 
         let self_ptr = Box::into_raw(self);
         let mut handle = Handle(0);
-        (uefi.BootServices.InstallProtocolInterface)(&mut handle, &GOP_POLICY_GUID, InterfaceType::Native, self_ptr as usize)?;
+        Result::from((uefi.BootServices.InstallProtocolInterface)(
+            &mut handle,
+            &GOP_POLICY_GUID,
+            InterfaceType::Native,
+            self_ptr as usize
+        ))?;
 
         //let _ = (uefi.BootServices.UninstallProtocolInterface)(handle, &GOP_POLICY_GUID, self_ptr as usize);
 
